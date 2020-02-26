2d976de8315f9a699d1c8a43a7b487d72534a47a
ZZFlying <47447170+ZZFlying@users.noreply.github.com>
2020-02-27 00:41:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,6 +13,9 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 git clone https://github.com/openwrt-dev/po2lmo.git
 pushd po2lmo
 make && sudo make install
