d8ab4460e9b69278dd7c2a524136f62724567b03
ZZFlying <47447170+ZZFlying@users.noreply.github.com>
2020-03-01 13:53:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,6 +12,11 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+wget http://www.zzflying.cn/luci-app-vlmcsd.tar.gz
+wget http://www.zzflying.cn/vlmcsd.tar.gz
+
+tar -zxf luci-app-vlmcsd.tar.gz -C package/
+tar -zxf vlmcsd.tar.gz -C package/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
