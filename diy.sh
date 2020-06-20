efeb7f4772a4626387c4d10f5e385bf31001a040
ZZFlying <47447170+ZZFlying@users.noreply.github.com>
2020-06-20 11:34:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,20 +8,20 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-wget http://www.zzflying.cn/luci-app-vlmcsd.tar.gz
-wget http://www.zzflying.cn/vlmcsd.tar.gz
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+#wget http://www.zzflying.cn/luci-app-vlmcsd.tar.gz
+#wget http://www.zzflying.cn/vlmcsd.tar.gz
 
-tar -zxf luci-app-vlmcsd.tar.gz -C package/
-tar -zxf vlmcsd.tar.gz -C package/
+#tar -zxf luci-app-vlmcsd.tar.gz -C package/
+#tar -zxf vlmcsd.tar.gz -C package/
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
+#git clone https://github.com/openwrt-dev/po2lmo.git
+#pushd po2lmo
+#make && sudo make install
+#popd
